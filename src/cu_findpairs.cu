be808db1a8ea593fa2fc6a1c980bb1116882882e
kamil <kamillys@gmail.com>
2014-08-10 23:16:38 +0200

    Indices extracted. Neighbours detected.

--- src/cu_findpairs.cu
+++ src/cu_findpairs.cu
@@ -1,28 +1,30 @@
 #include "cs_internal.h"
 #include "cudalib.h"
 #include <thrust/binary_search.h>
-#include <thrust/sequence.h>
 #include <thrust/count.h>
+#include <thrust/remove.h>
+#include <thrust/sequence.h>
 #include <bitset>
 
-#define CU_INVALID_INDEX_ITEM (static_cast<size_t>(~0))
+#define CU_INVALID_INDEX_ITEM (static_cast<u_int32_t>(~0))
 
 
 namespace cu
 {
 
 static texture<hashtype, 1, cudaReadModeElementType> cuFpCoordsTex;
+static texture<u_int32_t, 1, cudaReadModeElementType>   cuFpBndIdxTex;
 
 
 __device__
-size_t findLowerBound(hashtype val, size_t lb, size_t ub)
+size_t findLowerBound(hashtype val, u_int32_t lb, u_int32_t ub)
 {
     int32_t distance = ub;
     distance -= lb;
     while (distance > 0)
     {
-        size_t current = lb;
-        size_t step = distance / 2;
+        u_int32_t current = lb;
+        u_int32_t step = distance / 2;
         current += step;
         hashtype pt = tex1Dfetch(cuFpCoordsTex, current);
         if (pt < val) {
@@ -40,14 +42,14 @@ size_t findLowerBound(hashtype val, size_t lb, size_t ub)
 }
 
 __device__
-size_t findUpperBound(hashtype val, size_t lb, size_t ub)
+size_t findUpperBound(hashtype val, u_int32_t lb, u_int32_t ub)
 {
     int32_t distance = ub;
     distance -= lb;
     while (distance > 0)
     {
-        size_t current = lb;
-        size_t step = distance / 2;
+        u_int32_t current = lb;
+        u_int32_t step = distance / 2;
         current += step;
         hashtype pt = tex1Dfetch(cuFpCoordsTex, current);
         if (pt <= val) {
@@ -74,8 +76,8 @@ struct FindItem
     {
         //All are u_int32_t, size_t
         u_int32_t index = thrust::get<0>(t); // index
-        size_t lb = thrust::get<1>(t); // lower bound
-        size_t ub = thrust::get<2>(t); // upper bound
+        u_int32_t lb = thrust::get<1>(t); // lower bound
+        u_int32_t ub = thrust::get<2>(t); // upper bound
 
         if (lb == CU_INVALID_INDEX_ITEM || ub == CU_INVALID_INDEX_ITEM)
             return;
@@ -91,6 +93,25 @@ struct FindItem
         thrust::get<1>(t) = lb;
         thrust::get<2>(t) = ub;
         thrust::get<3>(t) = ub - lb;
+    }
+};
+
+struct PairIndexCleaner
+{
+    __device__
+    bool operator()(u_int32_t index)
+    {
+        u_int32_t val = tex1Dfetch(cuFpBndIdxTex, index);
+        return CU_INVALID_INDEX_ITEM == val;
+    }
+};
+
+struct PairIndexCopier
+{
+    __device__
+    u_int32_t operator()(u_int32_t index)
+    {
+        return tex1Dfetch(cuFpBndIdxTex, index);
     }
 };
 
@@ -112,12 +133,14 @@ static inline hashtype get_hash_mask(size_t spinquadCount, int sq_j, size_t part
 void locate_pairs(thrust::device_vector<float4>& spins,
                   std::vector<float>& spinquadrics, size_t spinquadCount)
 {
-    std::cerr << "SPINQUADS: " << spinquadCount << "\n";
-    //thrust::device_vector<u_int32_t> output(spins.size());
+    //std::cerr << "SPINQUADS: " << spinquadCount << "\n";
 
-    thrust::device_vector<size_t> lower_bound(spins.size());
-    thrust::device_vector<size_t> upper_bound(spins.size());
-    thrust::device_vector<size_t> differ_bound(spins.size());
+    std::vector<u_int32_t> output1;
+    std::vector<u_int32_t> output2;
+
+    thrust::device_vector<u_int32_t> lower_bound(spins.size());
+    thrust::device_vector<u_int32_t> upper_bound(spins.size());
+    thrust::device_vector<u_int32_t> differ_bound(spins.size());
 
     thrust::device_vector<hashtype> hashPart(spins.size());
     size_t parts = inc_div<size_t>(spinquadrics.size()/10, HASHBITS);
@@ -149,12 +172,12 @@ void locate_pairs(thrust::device_vector<float4>& spins,
 
 
             thrust::for_each(
-                        thrust::make_zip_iterator(thrust::make_tuple(thrust::counting_iterator<size_t>(0),
+                        thrust::make_zip_iterator(thrust::make_tuple(thrust::counting_iterator<u_int32_t>(0),
                                                                      lower_bound.begin(),
                                                                      upper_bound.begin(),
                                                                      differ_bound.begin()
                                                                      )),
-                        thrust::make_zip_iterator(thrust::make_tuple(thrust::counting_iterator<size_t>(spins.size()),
+                        thrust::make_zip_iterator(thrust::make_tuple(thrust::counting_iterator<u_int32_t>(spins.size()),
                                                                      lower_bound.end(),
                                                                      upper_bound.end(),
                                                                      differ_bound.begin()
@@ -164,10 +187,32 @@ void locate_pairs(thrust::device_vector<float4>& spins,
             cutilSafeCall( cudaUnbindTexture(cuFpCoordsTex) );
         }
         //TODO: Collect results
-        size_t count = lower_bound.size() - thrust::count(lower_bound.begin(), lower_bound.end(), CU_INVALID_INDEX_ITEM);
-        finalCount += count;
+        //size_t count = lower_bound.size() - thrust::count(lower_bound.begin(), lower_bound.end(), CU_INVALID_INDEX_ITEM);
+        //finalCount += count;
+
+        {
+            thrust::device_vector<u_int32_t> indices(spins.size());
+            thrust::sequence(indices.begin(), indices.end());
+            thrust::device_vector<u_int32_t> counterPairs;
+
+            cutilSafeCall( cudaBindTexture(NULL, cuFpBndIdxTex,
+                                           thrust::raw_pointer_cast(lower_bound.data()),
+                                           sizeof(u_int32_t)*lower_bound.size()) );
+            indices.resize(thrust::remove_if(indices.begin(), indices.end(), PairIndexCleaner()) - indices.begin());
+            //std::cerr << "COUNT<X>: " << indices.size() << std::endl;
+            counterPairs.resize(indices.size());
+            thrust::transform(indices.begin(), indices.end(), counterPairs.begin(), PairIndexCopier());
+            cutilSafeCall( cudaUnbindTexture(cuFpBndIdxTex) );
+
+            size_t currentSize = output1.size();
+            output1.resize(output1.size() + indices.size());
+            output2.resize(output2.size() + counterPairs.size());
+            thrust::copy(indices.begin(), indices.end(), output1.begin()+currentSize);
+            thrust::copy(counterPairs.begin(), counterPairs.end(), output2.begin()+currentSize);
+        }
     }
     std::cerr << "COUNT: " << finalCount << std::endl;
+    std::cerr << "SIZES: " << output1.size() << " " << output2.size() << std::endl;
 //    std::cerr << "Biggest diff: " <<
 //                 *(thrust::max_element(differ_bound.begin(), differ_bound.end()))
 //              << std::endl;
